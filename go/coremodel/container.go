8fd71ce44564a935c84b462a9fa593d44c14ad00
Zane Rockenbaugh <zane@liquid-labs.com>
2019-07-26 16:26:13 -0500

    leverage entity for new, clone, and clonenew

--- go/coremodel/container.go
+++ go/coremodel/container.go
@@ -3,3 +3,19 @@ package coremodel
 type Container struct {
   Entity
 }
+
+func NewContainer(
+    name string,
+    description string,
+    ownerPubID PublicID,
+    publiclyReadable bool) *Container {
+  return &Container{*NewEntity(name, description, ownerPubID, publiclyReadable)}
+}
+
+func (c *Container) Clone() *Container {
+  return &Container{*c.Entity.Clone()}
+}
+
+func (c *Container) CloneNew() *Container {
+  return &Container{*c.Entity.CloneNew()}
+}
