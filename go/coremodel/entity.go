02a75360d694c0db77733da60d06ee02557f9078
Zane Rockenbaugh <zane@liquid-labs.com>
2019-07-26 16:25:59 -0500

    add types for public and internal ID; update get/set helpers

--- go/coremodel/entity.go
+++ go/coremodel/entity.go
@@ -9,6 +9,9 @@ import (
   // "github.com/Liquid-Labs/catalyst-sql/go/catsql"
 )
 
+type InternalID int64
+type PublicID string
+
 // Entity is the basic Catalyst base-type. It is used for "independent" objects
 // and data. Any item which is directly retrievable and/or an authorization
 // target or subject must embed the Entity type. An Entity should be considered
@@ -17,14 +20,14 @@ import (
 type Entity struct {
   // Note, the ID is for internal use only and may or may not be set depending
   // in the source of the item (client or backend).
-  ID               int64       `json:"-" sql:",pk"`
-  PubID            string      `json:"pubId" pg:",unique,notnull"`
+  ID               InternalID  `json:"-" sql:",pk"`
+  PubID            PublicID    `json:"pubId" pg:",unique,notnull"`
   Name             string      `json:"name"`
   Description      string      `json:"description"`
-  OwnerID          int64       `json:"-" pg:",notnull"`
-  OwnerPubID       string      `json:"ownerPubId"`
+  OwnerID          InternalID  `json:"-" pg:",notnull"`
+  OwnerPubID       PublicID    `json:"ownerPubId"`
   PubliclyReadable bool        `json:"publiclyReadable" pg:",notnull"`
-  Containers       []Container `json:"containers" pg:"many2many:containers"`
+  Containers       []*Container `json:"containers" pg:"many2many:containers"`
   CreatedAt        time.Time   `json:createdAt`
   LastUpdated      time.Time   `json:"lastUpdated"`
   DeletedAt        time.Time   `pg:",soft_delete"`
@@ -32,37 +35,77 @@ type Entity struct {
 
 // catsql.ForQuery(`Entity`, func(q *orm.Query) *orm.Query { return q.Join(`JOIN entities ownerEntitiy ON entity.owner_id=ownerEntity.id`).ColumnExpr(`ownerEntity.pub_id AS owner_pub_id`)})
 
-/*func NewEntity(id int64,
-    pubID string,
+func NewEntity(
     name string,
-    ownerID int64,
-    publiclyReadable bool,
-    containers []int64,
-    createdAt time.Time,
-    lastUpdated time.Time,
-    deletedAt time.Time) *Entity {
-  return &Entity{id, pubID, name, ownerID, '', publiclyReadable, containers, createdAt, lastUpdated, deletedAt}
-}*/
-
-func (e *Entity) Clone() *Entity {
-  return &Entity{e.ID, e.PubID, e.Name, e.Description, e.OwnerID, e.OwnerPubID, e.PubliclyReadable, e.Containers, e.CreatedAt, e.LastUpdated, e.DeletedAt}
+    description string,
+    ownerPubID PublicID,
+    publiclyReadable bool) *Entity {
+  return &Entity{
+    0,
+    ``,
+    name,
+    description,
+    0,
+    ownerPubID,
+    publiclyReadable,
+    []*Container{},
+    time.Time{},
+    time.Time{},
+    time.Time{},
+  }
 }
 
-func (e *Entity) GetID() int64 { return e.ID }
+func (e *Entity) Clone() *Entity {
+  return &Entity{
+    e.ID,
+    e.PubID,
+    e.Name,
+    e.Description,
+    e.OwnerID,
+    e.OwnerPubID,
+    e.PubliclyReadable,
+    e.Containers,
+    e.CreatedAt,
+    e.LastUpdated,
+    e.DeletedAt,
+  }
+}
 
-func (e *Entity) GetPubID() string { return e.PubID }
+func (e *Entity) CloneNew() *Entity {
+  return &Entity{
+    0,
+    ``,
+    e.Name,
+    e.Description,
+    e.OwnerID,
+    e.OwnerPubID,
+    e.PubliclyReadable,
+    e.Containers,
+    e.CreatedAt,
+    e.LastUpdated,
+    e.DeletedAt,
+  }
+}
 
-func (e *Entity) GetOwnerID() int64 { return e.OwnerID }
+func (e *Entity) GetID() InternalID { return e.ID }
 
-func (e *Entity) GetOwnerPubID() string { return e.OwnerPubID }
-func (e *Entity) SetOwnerPubID(pid string) { e.OwnerPubID = pid }
+func (e *Entity) GetPubID() PublicID { return e.PubID }
+
+func (e *Entity) GetOwnerID() InternalID { return e.OwnerID }
+
+func (e *Entity) GetOwnerPubID() PublicID { return e.OwnerPubID }
+func (e *Entity) SetOwnerPubID(pid PublicID) { e.OwnerPubID = pid }
 
 func (e *Entity) IsPubliclyReadable() bool { return e.PubliclyReadable }
 func (e *Entity) SetPubliclyReadable(r bool) { e.PubliclyReadable = r }
 
+func (e *Entity) GetContainers() []*Container { return e.Containers }
+
 func (e *Entity) GetCreatedAt() time.Time { return e.CreatedAt }
 
 func (e *Entity) GetLastUpdated() time.Time { return e.LastUpdated }
+
+func (e *Entity) GetDeletedAt() time.Time { return e.DeletedAt }
 
 // EntityIface is provided for extension by abstract entity types. In most
 // situations you will use the Entity struct directly.
