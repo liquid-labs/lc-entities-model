2170e5799a7c3cbafc2df3dea879d8e66dca0a9a
Zane Rockenbaugh <zane@liquid-labs.com>
2019-07-26 16:26:26 -0500

    update tests and introduce go.mod

---
+++ go/coremodel/entity_test.go
@@ -0,0 +1,94 @@
+package coremodel_test
+
+import (
+  "reflect"
+  "testing"
+  "time"
+
+  . "github.com/Liquid-Labs/catalyst-core-model/go/coremodel"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestEntitiesClone(t *testing.T) {
+  now := time.Now()
+  orig := NewEntity(`john`, `cool`, `owner-A`, true)
+  orig.ID = 1
+  orig.PubID = `abc`
+  orig.OwnerID = 2
+  orig.Containers = []*Container{}
+  orig.CreatedAt = now
+  orig.LastUpdated = now.Add(100)
+  orig.DeletedAt = now.Add(200)
+  clone := orig.Clone()
+
+  assert.Equal(t, orig, clone, "Clone does not match.")
+
+  clone.ID = 3
+  clone.PubID = `hij`
+  clone.Name = `sally`
+  clone.Description = `awesome`
+  clone.OwnerID = 4
+  clone.OwnerPubID = `owner-B`
+  clone.PubliclyReadable = false
+  clone.Containers = []*Container{NewContainer(`folder`, `a folder`, `owner-B`, false)}
+  clone.CreatedAt = orig.CreatedAt.Add(20)
+  clone.LastUpdated = orig.LastUpdated.Add(20)
+  clone.DeletedAt = orig.DeletedAt.Add(20)
+
+  // TODO: abstract this
+  oReflection := reflect.ValueOf(orig).Elem()
+  cReflection := reflect.ValueOf(clone).Elem()
+  for i := 0; i < oReflection.NumField(); i++ {
+    assert.NotEqualf(
+      t,
+      oReflection.Field(i).Interface(),
+      cReflection.Field(i).Interface(),
+      `Fields '%s' unexpectedly match.`,
+      oReflection.Type().Field(i),
+    )
+  }
+}
+
+func TestEntitiesCloneNew(t *testing.T) {
+  now := time.Now()
+  orig := NewEntity(`john`, `cool`, `owner-A`, true)
+  orig.ID = 1
+  orig.PubID = `abc`
+  orig.OwnerID = 2
+  orig.Containers = []*Container{}
+  orig.CreatedAt = now
+  orig.LastUpdated = now.Add(100)
+  orig.DeletedAt = now.Add(200)
+  clone := orig.CloneNew()
+
+  assert.Equal(t, InternalID(0), clone.ID)
+  assert.Equal(t, PublicID(``), clone.PubID)
+  clone.ID = 1
+  clone.PubID = `abc`
+  assert.Equal(t, orig, clone, "Clone does not match.")
+
+  clone.ID = 3
+  clone.PubID = `hij`
+  clone.Name = `sally`
+  clone.Description = `awesome`
+  clone.OwnerID = 4
+  clone.OwnerPubID = `owner-B`
+  clone.PubliclyReadable = false
+  clone.Containers = []*Container{NewContainer(`folder`, `a folder`, `owner-B`, false)}
+  clone.CreatedAt = orig.CreatedAt.Add(20)
+  clone.LastUpdated = orig.LastUpdated.Add(20)
+  clone.DeletedAt = orig.DeletedAt.Add(20)
+
+  // TODO: abstract this
+  oReflection := reflect.ValueOf(orig).Elem()
+  cReflection := reflect.ValueOf(clone).Elem()
+  for i := 0; i < oReflection.NumField(); i++ {
+    assert.NotEqualf(
+      t,
+      oReflection.Field(i).Interface(),
+      cReflection.Field(i).Interface(),
+      `Fields '%s' unexpectedly match.`,
+      oReflection.Type().Field(i),
+    )
+  }
+}
---
+++ go/go.mod
@@ -0,0 +1,6 @@
+module github.com/Liquid-Labs/catalyst-core-model/go
+
+require (
+	github.com/Liquid-Labs/go-nullable-mysql v1.0.2
+	github.com/stretchr/testify v1.3.0
+)
