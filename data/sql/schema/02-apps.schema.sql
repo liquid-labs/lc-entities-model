ab4d3cad4dbf938b6f902d40ca7bacbea3e9cf0a
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-05 16:30:19 -0500

    factor out user and location data models; update name
    name from 'entity' to more standard 'entities'

--- data/sql/schema/02-apps.schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE apps (
-  id BIGSERIAL,
-  name VARCHAR(128) NOT NULL,
-  type VARCHAR(64) NOT NULL,
-  CONSTRAINT apps_key PRIMARY KEY ( id ),
-  CONSTRAINT apps_ref_entities FOREIGN KEY ( id ) REFERENCES entities ( id )
-);
