7be9678d48cdb08f23cef1ec0a86de417962122c
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-14 01:08:05 -0500

    add 'ResourceName' and 'Identifiable'

--- go/entities/model.go
+++ go/entities/model.go
@@ -8,6 +8,7 @@ import (
 
 type InternalID int64
 type PublicID string
+type ResourceName string
 
 // Entity is the base type for all independent entities in the Liquid Code
 // model. Any item which is directly retrievable, an authorization target, or
@@ -15,30 +16,35 @@ type PublicID string
 // considered an "abstract" type and never created directly, but only as part of
 // a concrete, final type.
 type Entity struct {
+  tableName        struct{}     `sql:"entities,alias:entity,select:entities_owner_pub_id"`
   // Note, the ID is for internal use only and may or may not be set depending
   // in the source of the item (client or backend).
-  ID               InternalID  `json:"-" sql:",pk"`
-  PubID            PublicID    `json:"pubId" pg:",unique,notnull"`
-  Name             string      `json:"name"`
-  Description      string      `json:"description"`
-  OwnerID          InternalID  `json:"-" pg:",notnull"`
-  OwnerPubID       PublicID    `json:"ownerPubId"`
-  PubliclyReadable bool        `json:"publiclyReadable" pg:",notnull"`
-  CreatedAt        time.Time   `json:createdAt`
-  LastUpdated      time.Time   `json:"lastUpdated"`
-  DeletedAt        time.Time   `pg:",soft_delete"`
+  ID               InternalID   `json:"-" sql:",pk"`
+  PubID            PublicID     `json:"pubId" pg:",unique,notnull"`
+  ResourceName     ResourceName `json:"resourceName"`
+  Name             string       `json:"name"`
+  Description      string       `json:"description"`
+  OwnerID          InternalID   `json:"-"`
+  OwnerPubID       PublicID     `json:"ownerPubId"`
+  PubliclyReadable bool         `json:"publiclyReadable" pg:",notnull"`
+  CreatedAt        time.Time    `json:createdAt`
+  LastUpdated      time.Time    `json:"lastUpdated"`
+  DeletedAt        time.Time    `pg:",soft_delete"`
 }
 
 // catsql.ForQuery(`Entity`, func(q *orm.Query) *orm.Query { return q.Join(`JOIN entities ownerEntitiy ON entity.owner_id=ownerEntity.id`).ColumnExpr(`ownerEntity.pub_id AS owner_pub_id`)})
 
 func NewEntity(
+    exemplar Identifiable,
     name string,
     description string,
     ownerPubID PublicID,
     publiclyReadable bool) *Entity {
   return &Entity{
-    0,
-    ``,
+    struct{}{},
+    exemplar.GetID(),
+    exemplar.GetPubID(),
+    exemplar.GetResourceName(),
     name,
     description,
     0,
@@ -52,8 +58,10 @@ func NewEntity(
 
 func (e *Entity) Clone() *Entity {
   return &Entity{
+    struct{}{},
     e.ID,
     e.PubID,
+    e.ResourceName,
     e.Name,
     e.Description,
     e.OwnerID,
@@ -69,12 +77,17 @@ func (e *Entity) CloneNew() *Entity {
   newE := e.Clone()
   newE.ID = 0
   newE.PubID = ``
+  newE.CreatedAt = time.Time{}
+  newE.LastUpdated = time.Time{}
+  newE.DeletedAt = time.Time{}
   return newE
 }
 
 func (e *Entity) GetID() InternalID { return e.ID }
 
 func (e *Entity) GetPubID() PublicID { return e.PubID }
+
+func (e *Entity) GetResourceName() ResourceName { return e.ResourceName }
 
 func (e *Entity) GetOwnerID() InternalID { return e.OwnerID }
 
@@ -90,24 +103,8 @@ func (e *Entity) GetLastUpdated() time.Time { return e.LastUpdated }
 
 func (e *Entity) GetDeletedAt() time.Time { return e.DeletedAt }
 
-// EntityIface is provided for extension by abstract entity types. In most
-// situations you will use the Entity struct directly.
-// TODO: confirm no longer necessary.
-/*type EntityIface interface {
-  GetID() int64
-
-  GetPubID() string
-
-  GetOwnerID() string
-  // The owner ID is always set based on the OwnerPubID when storing.
-
-  GetOwnerPubID() string
-  SetOwnerPubID(string)
-
-  IsPubliclyReadable() bool
-  SetPubliclyReadable(bool)
-
-  GetCreatedAt() time.Time
-
-  GetLastUpdated() time.Time
-}*/
+type Identifiable interface {
+  GetID() InternalID
+  GetPubID() PublicID
+  GetResourceName() ResourceName
+}
